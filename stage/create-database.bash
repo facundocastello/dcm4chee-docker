0821a9b502a3d7bd81f2dbc3f2ccfff8a2e9a218
facundo <facundoacastello@gmail.com>
2019-08-08 14:34:55 -0300

    add dcm4chee folder

--- stage/create-database.bash
+++ stage/create-database.bash
@@ -10,7 +10,7 @@ mysql -uroot < /stage/create_dcm4chee_databases.sql
 # Load the 'pacsdb' database schema
 mysql -upacs -ppacs pacsdb < $DCM_DIR/sql/create.mysql
 
-sed "s/type=/engine=/g" $ARR_DIR/sql/dcm4chee-arr-mysql.ddl > fixed.ddl
-mv fixed.ddl $ARR_DIR/sql/dcm4chee-arr-mysql.ddl
+sed "s/type=/engine=/g" $DCM_DIR/sql/dcm4chee-arr-mysql.ddl > fixed.ddl
+mv fixed.ddl $DCM_DIR/sql/dcm4chee-arr-mysql.ddl
 # Load the 'arrdb' database schema
-mysql -uarr -parr arrdb < $ARR_DIR/sql/dcm4chee-arr-mysql.ddl
+mysql -uarr -parr arrdb < $DCM_DIR/sql/dcm4chee-arr-mysql.ddl
